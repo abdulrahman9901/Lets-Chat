1ebf69cfa16df6ec9a9184694d3d716f76e24bd3
Abdulrahman Badr <41681016+abdulrahman9901@users.noreply.github.com>
2026-03-16 01:38:30 +0200

    fix: remove erroneous tkinter import from chat.models

--- src/chat/models.py
+++ src/chat/models.py
@@ -1,6 +1,5 @@
 from email import message
 from email.policy import default
-from tkinter import CASCADE
 from django.db import models
 #from django.contrib.auth import get_user_model
 from django.db import models
