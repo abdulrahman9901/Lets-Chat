f5b2b142522e6e98d3246b331fa9e9a659b16418
Abdulrahman Badr <41681016+abdulrahman9901@users.noreply.github.com>
2026-03-16 18:15:36 +0200

    chore(logging): write console logs to stdout

--- src/Justchat/settings.py
+++ src/Justchat/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import base64
 import hashlib
 import os
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -259,6 +260,7 @@ LOGGING = {
         'console': {
             'class': 'logging.StreamHandler',
             'formatter': 'simple',
+            'stream': sys.stdout,
         },
         'file_django': {
             'class': 'logging.handlers.RotatingFileHandler',
